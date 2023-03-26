27244f15380ed204fa2995891470e71798e851e7
Honza Klimeš <johny.klimes@seznam.cz>
2023-03-27 00:24:10 +0200

    feat: working on android

--- App.js
+++ App.js
@@ -1,11 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+import axios from "axios";
+import formData from "form-data";
 
 export default function App() {
+  const [startCamera, setStartCamera] = React.useState(false);
+  let camera;
+
+  const __startCamera = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    if (status === "granted") {
+      setStartCamera(true);
+    } else {
+      Alert.alert("Access denied");
+    }
+  };
+
+  const __takePicture = async () => {
+    if (!camera) return;
+    const photo = await camera.takePictureAsync({ base64: true });
+    var bodyFormData = new FormData();
+    bodyFormData.append("image", {
+      uri: photo.uri,
+      name: "photo.jpg",
+      type: "image/jpeg",
+    });
+    axios({
+      method: "post",
+      maxBodyLength: Infinity,
+      headers: { "Content-Type": "multipart/form-data" },
+      data: bodyFormData,
+      url: "http://janklimes.eu:3000/",
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {startCamera ? (
+        <Camera
+          style={{ flex: 1, width: "100%" }}
+          ref={(r) => {
+            camera = r;
+          }}
+        >
+          <View
+            style={{
+              position: "absolute",
+              bottom: 0,
+              flexDirection: "row",
+              flex: 1,
+              width: "100%",
+              padding: 20,
+              justifyContent: "space-between",
+            }}
+          >
+            <View
+              style={{
+                alignSelf: "center",
+                flex: 1,
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity
+                onPress={__takePicture}
+                style={{
+                  width: 70,
+                  height: 70,
+                  bottom: 0,
+                  borderRadius: 50,
+                  backgroundColor: "#fff",
+                }}
+              />
+            </View>
+          </View>
+        </Camera>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "#fff",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            onPress={__startCamera}
+            style={{
+              width: 130,
+              borderRadius: 4,
+              backgroundColor: "#14274e",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              height: 40,
+            }}
+          >
+            <Text
+              style={{
+                color: "#fff",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              Take picture
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +122,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
